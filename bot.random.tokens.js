1bcc63c98d6f4721525bd4228e14f100ecda4d31
Suissinha <jnascimento@gmail.com>
2023-04-05 16:20:29 -0300

    Bot com tokens randomicos

--- bot.random.tokens.js
+++ bot.random.tokens.js
@@ -5,8 +5,9 @@ const provider = new JsonRpcProvider(`https://testnet.doric.network/`)
 const axios = require('axios');
 
 const apiKey = '7XVPRP4R6VM22S65BCU62TB4Z8F678DZ9Z';
-const pk = "dc0c176a2c4f3f5bb4f4eee985f4a8f0503249127fa992138059f9cbfea71681"
-const walletJson = new ethers.Wallet(pk);
+const pk1 = "14c16c6e97885e553482d63fb3365ad96e0727b6bedd12362b8c691162532637"
+const pk2 = "dc0c176a2c4f3f5bb4f4eee985f4a8f0503249127fa992138059f9cbfea71681"
+const walletJson = new ethers.Wallet(pk1);
 const signer = walletJson.connect(provider);
 
 // account2 com drc 0xa620A5199F498B81191D291c62a70aa761be7536
@@ -28,7 +29,17 @@ const account1 = "0x4e238622c1797115F35174C50583F5D41b915cb6";
 const account2 = "0xa620A5199F498B81191D291c62a70aa761be7536"
 
 // address do contrato Doric
-const address = "0x61b0c854C60a5577E68be7a40Bc4D61935c1a058"
+// const address = "0x61b0c854C60a5577E68be7a40Bc4D61935c1a058"
+const token01 = "0x21Ec14ecAA44A036132Ea6A267B4963D41D8E9D6"
+const token02 = "0xb386f940ddbB9CBB0aa909bc34325b13F5E4eAb9"
+const token03 = "0x8Ad80085ec08CceDB99CDb1986BcbD72d1Ef6cc0"
+const token04 = "0xB38013EB44D6496D50Abbf266c39b16761b7ba52"
+const token05 = "0xcB7d6CfFb8A10156Ee533A46a2D75AD9d3425c9E"
+const token06 = "0xdB4029Eabe19C2ED45257D6df2b158aA8B55096c"
+const token07 = "0xDd84078531bcb4788Acf25bBD60FC900C990827b"
+const tokenAddresses = [
+  token01, token02, token03, token04, token05, token06, token07
+]
 // const address = "0x94b6dAE0E72da0F2f076e44b8B819723Fe1d8a40";
 // address do contrato BRZ
 const contractAddress = '0x420412e765bfa6d85aaac94b4f7b708c89be2e2b';
@@ -38,8 +49,9 @@ let tempo = 0;
 
 async function atualizaTempo() {
   // Gera um tempo aleatório entre 1 min e 1 hora
-  // tempo = Math.floor(Math.random() * (6000000 - 800000 + 1)) + 10000;
-  tempo = Math.floor(Math.random() * (1000000 - 300000 + 1)) // + 10000;
+  tempo = Math.floor(Math.random() * (259200000 - 800000 + 1)) + 10000;
+  // tempo = Math.floor(Math.random() * (1000000 - 300000 + 1)) // + 10000;
+  // tempo = Math.floor(Math.random() * (60000 - 20000 + 1)) // + 10000;
   console.log(`\n\n\nAntes: ${new Date()}`);
   console.log(`Próximo intervalo: ${tempo / 1000}s`);
   // Define o próximo intervalo
@@ -51,7 +63,8 @@ async function atualizaTempo() {
   console.log(`Depois rodo o bot: ${new Date()}`);
 
   try {
-    const erc20 = new ethers.Contract(address, abi, signer);
+    const randomAddress = tokenAddresses[Math.floor(Math.random() * tokenAddresses.length)];
+    const erc20 = new ethers.Contract(randomAddress, abi, signer);
     console.log(await erc20.symbol());
     const randomValue = (Math.floor(Math.random() * 100) + 1).toString()
     console.log({randomValue});
